ebc1db5f7c50228425370c957c2af8eaf831338f
Alex Cleveland <Alexcleveland5@gmail.com>
2022-11-25 17:57:22 -0700

    Tasks start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import { useState } from "react"
+import React from "react"
 import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Clean Car',
+      day: 'Nov 23rd at 2:30PM',
+      reminder: true,
+    }
+
+  ])
 
 
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
