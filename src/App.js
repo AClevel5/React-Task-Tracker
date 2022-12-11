d22bff5864f872b1bd11eecd5e8831e3568e732f
Alex Cleveland <Alexcleveland5@gmail.com>
2022-12-10 17:50:39 -0700

    Toggle Reminder

--- src/App.js
+++ src/App.js
@@ -10,18 +10,35 @@ function App() {
       text: 'Clean Car',
       day: 'Nov 23rd at 2:30PM',
       reminder: true,
-    }
+    },
+    {
+      id: 2,
+      text: 'Wash Towels',
+      day: 'Nov 24rd at 2:30PM',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Do Dishes',
+      day: 'Nov 25rd at 2:30PM',
+      reminder: true,
+    },
+
   ]);
 
   const deleteTask = (id) => { setTasks(tasks.filter((task) => task.id !== id)) }
 
-
+  const toggleReminder = (id) => {
+    console.log(id)
+    setTasks(tasks.map((task) => task.id === id ?
+      { ...task, reminder: !task.reminder } : task))
+  }
 
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} /> : 'No Tasks'}
     </div>
   );
 }
