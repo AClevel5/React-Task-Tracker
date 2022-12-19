4141057426e7c621810f05f8adbb539c15eba4e4
Alex Cleveland <Alexcleveland5@gmail.com>
2022-12-19 16:59:17 -0700

    Create, Read, Delete routes

--- src/App.js
+++ src/App.js
@@ -23,12 +23,29 @@ function App() {
     return data
   }
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
   }
-  const deleteTask = (id) => { setTasks(tasks.filter((task) => task.id !== id)) }
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   const toggleReminder = (id) => {
     console.log(id)
