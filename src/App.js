c92b5d0278fdf11ef28781f25d0719dadd3f3298
Alex Cleveland <Alexcleveland5@gmail.com>
2022-12-19 16:49:31 -0700

    Json Server start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import React from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
@@ -6,27 +6,23 @@ import AddTask from "./components/AddTask"
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Clean Car',
-      day: 'Nov 23rd at 2:30PM',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Wash Towels',
-      day: 'Nov 24rd at 2:30PM',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Do Dishes',
-      day: 'Nov 25rd at 2:30PM',
-      reminder: true,
-    },
+  const [tasks, setTasks] = useState([]);
 
-  ]);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
